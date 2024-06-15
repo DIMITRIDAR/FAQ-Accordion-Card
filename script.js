2a0e97f8e8d2f4c6c57e86a475116d63be1c3e6e
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-06-15 10:25:19 +0400

    Finished version of the JS code

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let active = null;
   let mainItems = document.querySelectorAll(".faq-item");
   mainItems.forEach(function (mainItem) {
     mainItem.addEventListener("click", function (e) {
       let faqAnswer = mainItem.querySelector(".faq-answer");
-      if (faqAnswer.style.display === "block") {
-        faqAnswer.style.display = "none";
-        mainItem.querySelector(".arrow").style.transform = "rotate(0deg)";
-        mainItem.querySelector(".faq-question").style.fontWeight = "400";
-      } else {
+      if (active !== null) {
+        active.querySelector(".faq-answer").style.display = "none";
+        active.querySelector(".arrow").style.transform = "rotate(0deg)";
+        active.querySelector(".faq-question").style.fontWeight = "400";
+      }
+      if (active !== mainItem) {
         faqAnswer.style.display = "block";
         mainItem.querySelector(".arrow").style.transform = "rotate(180deg)";
         mainItem.querySelector(".faq-question").style.fontWeight = "700";
+        active = mainItem;
+      } else {
+        faqAnswer.style.display = "none";
+        mainItem.querySelector(".arrow").style.transform = "rotate(0deg)";
+        mainItem.querySelector(".faq-question").style.fontWeight = "400";
+        active = null;
       }
     });
   });
